799572d0152a5931d06c4fb10de6a7858b7d9bdc
Douglas Arruda <322899190+arddouglas1-web@users.noreply.github.com>
2026-09-16 12:50:42 -0300

    feat: versão inicial

---
+++ README.md
@@ -0,0 +1,9 @@
+"Medidor de consumdo de água"
+
+Desenvolvido por ![Douglas Arruda](https://img.shields.io/badge/Douglas_Arruda-red)
+
+O objetivo deste programa é mostrar como está o consumo de água nas casas e comércio.
+Primeiro o usuário digita qual o tipo de residência e qual o consumo de água em metros cúbicos.
+O programa irá calcula se o consumo é econômico, moderado ou se está acima do limite
+
+Programa desenvolvido em ![Python](https://img.shields.io/badge/Python-3.14-%233776AB?style=flat&logo=python&logoColor=white)
---
+++ app.py
@@ -0,0 +1,23 @@
+# Medidor de consumdo de água
+# Douglas Arruda
+
+# Entrada de Dados
+imovel_tipo = input("Digite o tipo de imóvel (casa, apartamento ou comercial) ")
+consumo_agua = float(input("Digite o consumo de água mensal (m3) " ))
+
+# Processamento de Dados
+if      imovel_tipo == "comercial":
+        print ("Tarifa comercial aplicada, consulte o plano corporativo.")
+elif    imovel_tipo  == "apartamento" and consumo_agua < 10: 
+        print ("Consumo econômico, excelente controle de água!")
+elif    imovel_tipo == "apartamento" or (imovel_tipo == "casa" and consumo_agua < 25) :
+        print ("Consumo moderado, dentro do padrão residencial.") 
+else:
+        print  ("Consumo acima do limite residencial")      
+
+        
+          
+        
+    
+
+
